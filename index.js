f8779948505267edb3797623e7b78ccb9623b768
David <david123rana@gmail.com>
2023-11-09 17:09:44 +0530

    create server for app

--- index.js
+++ index.js
@@ -3,14 +3,26 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from "cors";
+import { Server } from 'socket.io';
+import http from 'http'
 
 import ItemRouter from "./server/ItemRoute.js";
 import Routes from "./server/route.js";
 import Auth from "./server/authRouter.js";
 import secureRoutes from './server/secureRoutes.js';
+import AppRouter from './server/appRouter.js';
 
 const app = express();
 dotenv.config({ path: "config.env" }); // Load the config.env file
+const MainServer = http.createServer(app);
+const io = new Server(MainServer, {
+  cors: {
+    origin: "*",
+    // methods: ["GET", "POST"],
+    // allowedHeaders: ["my-custom-header"],
+    // credentials: true
+  }
+});
 
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,9 +32,20 @@ app.use("/info", ItemRouter);
 app.use("/record", Routes);
 app.use("/auth", Auth);
 app.use('/secure', secureRoutes);
+app.use('/app', AppRouter);
 
 const PORT = process.env.PORT || "8080";
 const URL = process.env.DATABASE;
+
+io.on('connection', (socket) => {
+  socket.on('message', (data) => {
+    io.emit('message', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('A user disconnected');
+  });
+});
 
 mongoose
   .connect(URL, {
@@ -31,7 +54,7 @@ mongoose
     useFindAndModify: false,
   })
   .then(() => {
-    app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
+    MainServer.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
   })
   .catch((error) => {
     console.log("Error:", error.message);
